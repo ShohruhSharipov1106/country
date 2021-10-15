44e973a93724019b17e958c7628dc118fc96a70d
Shohruh_Sharipov <80044583+ShohruhSharipov1106@users.noreply.github.com>
2021-10-15 19:30:41 +0500

    nimadir yoziladi

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         primarySwatch: Colors.blue,
       ),
       home: const Country(),
